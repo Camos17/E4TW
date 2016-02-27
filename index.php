0295083d5f3c31852ec2fec57fb1af27acb6b0bf
Familiar2 <Familiar2>
2016-02-26 19:27:59 -0500

    First Commit

---
+++ index.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="es">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="x-ua-compatible" content="ie=edge">
+		<title>Entrepeneurs for the world</title>
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<link rel="apple-touch-icon" href="apple-touch-icon.png">
+		<!-- Place favicon.ico in the root directory -->
+
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/font-awesome.min.css">
+		<link rel="stylesheet" href="css/style.css">
+	</head>
+	<body>
+		<div class="col-xs-8 col-xs-offset-2 col-sm-6 col-sm-offset-3 col-md-4 col-md-offset-4 col-lg-2 col-lg-offset-5 logo">
+			<img class="img-responsive" src="img/Logo.svg" alt="Entrepeneurs for the world">	
+		</div>
+		<div id="video-container" class="col-xs-12 col-md-6 col-md-offset-3">
+			<iframe width="854" height="480" src="https://www.youtube.com/embed/SAlU5mTnKRU" frameborder="0" allowfullscreen></iframe>
+			<img class="img-responsive" src="img/BarraVideo.svg" alt="barravideo">
+		</div>
+
+
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/jquery.fitvids.js"></script>
+		<script type="text/javascript">
+			$("#video-container").fitVids({ ignore: '.mycooldiv, #myviiid'});
+		</script>
+
+	</body>
+	
+	<footer>
+		<ul class="col-xs-12 col-sm-4 col-sm-offset-4" id="redes-sociales"> 
+			<li>
+				<a href="http://facebook.com">
+					<i class="fa fa-facebook-square"></i>
+				</a>
+			</li>
+			<li>
+				<a href="http://twitter.com">
+					<i class="fa fa-twitter-square"></i>
+				</a>
+			</li>
+			<li>
+				<a href="https://plus.google.com/">
+					<i class="fa fa-instagram"></i>
+				</a>
+			</li>
+		</ul>
+			</div>	
+	</footer>
+	
+</html>	
+
